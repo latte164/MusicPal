cc82c43c003bd5b3703181526f7c1d4f22a10a0d
latte164 <latta164@gmail.com>
2019-06-24 13:05:34 -0400

    Initial commit

---
+++ app/src/main/java/com/lattestudios/william/musicpal/MainActivity.java
@@ -0,0 +1,46 @@
+package com.lattestudios.william.musicpal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_lists:
+                    return true;
+                case R.id.navigation_search:
+
+                    // Start new activity
+                    Intent myIntent = new Intent(getApplicationContext(),SearchActivity.class);
+                    startActivity(myIntent);
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_USE_LOGO | ActionBar.DISPLAY_SHOW_HOME);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setIcon(R.drawable.logo_whitebar);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
---
+++ app/src/main/java/com/lattestudios/william/musicpal/SearchActivity.java
@@ -0,0 +1,48 @@
+package com.lattestudios.william.musicpal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class SearchActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_lists:
+
+                    // Start new activity
+                    Intent myIntent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(myIntent);
+                    overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+
+                    return true;
+                case R.id.navigation_search:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_USE_LOGO | ActionBar.DISPLAY_SHOW_HOME);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setIcon(R.drawable.logo_whitebar);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
